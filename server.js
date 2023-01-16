bfada226c9c702e9ba5b3078b7ef61cf0ef5f4d6
fadingNA <94852295+fadingNA@users.noreply.github.com>
2023-01-16 00:13:34 -0500

    push to cyclic

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const express = require('express');
+/*********************************************************************************
+ *  WEB422 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Nonthachai Plodthong Student ID: 152487211 Date: 16/Jan/2023
+ *  Cyclic Link: _______________________________________________________________
+ *
+ ********************************************************************************/
 
+
+const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
