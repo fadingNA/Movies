7f896c2b757197b43123e2bdad4ba91fe5b55f59
fadingNA <94852295+fadingNA@users.noreply.github.com>
2023-01-24 00:55:17 -0500

    add Assignment2

--- js/main.js
+++ js/main.js
@@ -2,36 +2,64 @@ let page = 1;
 let perPage = 10;
 
 const loadMovieData = (title = null) => {
+
     let endpoint = 'https://exuberant-trench-coat-wasp.cyclic.app';
     let qp = title ? `${endpoint}/api/movies?page=${page}&perPage=${perPage}&title=${title}` :
         `${endpoint}/api/movies?page=${page}&perPage=${perPage}`;
-    document.querySelector(".pagination").classList.remove("d-none");
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(data => {
-            // handle the data here, for example:
-            let movies = data.movies;
-            let tbody = document.querySelector('#moviesTable tbody');
-            tbody.innerHTML = '';
-            movies.forEach(movie => {
-                let tr = document.createElement('tr');
-                tr.innerHTML = `
-                    <td>${movie.year}</td>
-                    <td>${movie.title}</td>
-                    <td>${movie.plot}</td>
-                    <td>${movie.rating}</td>
-                    <td>${movie.runtime}</td>`;
-                tbody.appendChild(tr);
-            });
-        })
-        .catch(error => {
-            // handle the error here
-            console.error(error);
+
+    if(title){
+        document.querySelector(".pagination").classList.add("d-none");
+    }else{
+        document.querySelector(".pagination").classList.remove("d-none");
+    }
+    fetch(qp).then((res) => res.json()).then((data) => {
+        console.log(data)
+        let movieList = "";
+        data.forEach(movie => {
+            movieList += `
+          <tr data-id="${movie._id}">
+            <td>${movie.year}</td>
+            <td>${movie.title}</td>
+            <td>${movie.fullplot}</td>
+            <td>${movie.rated}</td>
+            <td>${Math.floor(movie.runtime / 60)} hour ${(movie.runtime % 60).toString().padStart(2, '0')} minutes</td> 
+          </tr>
+        `;
         });
+        // Add the formatted movie data to the DOM
+        document.querySelector('#moviesTable tbody').innerHTML = movieList;
+        $('#moviesTable tbody').html(movieList);
+        // document.querySelector("#current-page").innerHTML = page;
+        document.querySelectorAll('#moviesTable tbody tr').forEach((row) => {
+            row.addEventListener('click', (e) => {
+                let clicked = row.getAttribute('data-id');
+                fetch(`${endpoint}/api/movies/${clicked}`).then((res) => res.json()).then((data) => {
+                    document.querySelector(`#detailsModal .modal-title`).innerHTML = data.title;
+                    document.querySelector(`#detailsModal .modal-body`).innerHTML = `
+                        <img src="${data.poster}" style="{justify-content: center} alt="poster"/> 
+                        <p>Director: ${data.directors.join(', ')}</p> 
+                        <p>Fullplot: ${data.fullplot}</p> 
+                        <p>Cast: ${data.cast ? data.cast.join(', ') : 'No data'}</p> 
+                        <p>Nominated: ${data.awards.text}</p> 
+                        <p>IMDB RATING: ${data.imdb.rating}</p>
+                        <p>IMDB VOTES: ${data.imdb.votes}</p>;`
+                    $(`#detailsModal`).modal(`show`);
+                    $('.modal-footer').click(function(){
+                        $('#detailsModal').modal('hide');
+                    });
+                })
+            })
+        })
+    })
+    document.querySelector(".pagination").classList.remove("d-none");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    loadMovieData();
     // attach event listeners for pagination and search here
+    document.querySelector("#cur-p").addEventListener("click", () => {
+        loadMovieData();
+    });
     document.querySelector("#p-page").addEventListener("click", () => {
         if (page > 1) {
             page--;
